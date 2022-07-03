59bd578b975d4fe8a6dbc6e2b57ac7756214e177
Alexander Latyshev <imposya0@gmail.com>
2022-07-04 00:35:27 +0300

    first CRUD app CREATE

--- src/main/java/ru/imposya/firstcrudapp/controllers/PeopleController.java
+++ src/main/java/ru/imposya/firstcrudapp/controllers/PeopleController.java
@@ -3,11 +3,9 @@ package ru.imposya.firstcrudapp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.imposya.firstcrudapp.dao.PersonDAO;
+import ru.imposya.firstcrudapp.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -30,5 +28,16 @@ public class PeopleController {
                        Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
